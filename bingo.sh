81678059cb7a72966a27b854c1d17af68c733dbc
Logan Newsom <lrnewsom@ksu.edu>
2024-07-27 19:17:51 +0000

    Save progress: 1010.1

--- bingo.sh
+++ bingo.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 # Bingo Number Generator
+
 echo -e "\n~~ Bingo Number Generator ~~\n"
+
 NUMBER=$(( RANDOM % 75 + 1 ))
 TEXT="The next number is, "
+
 if (( NUMBER <= 15 ))
 then
   echo $TEXT B:$NUMBER
